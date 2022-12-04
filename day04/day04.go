d931f882ae4cdf3a465ef13e2f409512d0c15c33
zen <zenyui@gmail.com>
2022-12-04 16:09:05 -0600

    feat: complete day 4

--- day04/day04.go
+++ day04/day04.go
@@ -1,23 +1,83 @@
 package day04
 
-const (
-	inputPath = "./day03/input.txt"
+import (
+	"fmt"
+	"strconv"
+	"strings"
+
+	"github.com/zenyui/aoc-22/util"
 )
 
+const (
+	inputPath = "./day04/input.txt"
+)
+
+// Run part 4
 func Run() error {
-	if err := part1(); err != nil {
+	lines, err := util.ReadFile(inputPath)
+	if err != nil {
 		return err
 	}
-	if err := part2(); err != nil {
-		return err
+	numSubsets := 0
+	numOverlaps := 0
+	for line := range lines {
+		intRanges, err := lineToRanges(line)
+		if err != nil {
+			return err
+		}
+		if len(intRanges) != 2 {
+			return fmt.Errorf("malformed line %s", line)
+		}
+		if isOverlap(intRanges[0], intRanges[1]) {
+			numOverlaps += 1
+			if isSubset(intRanges[0], intRanges[1]) {
+				numSubsets += 1
+			}
+		}
 	}
+	fmt.Printf("part 1: %d\n", numSubsets)
+	fmt.Printf("part 2: %d\n", numOverlaps)
 	return nil
 }
 
-func part1() error {
-	return nil
+// inclusiveRange represents an inclusive range of ints
+type inclusiveRange struct {
+	Start int
+	End   int
 }
 
-func part2() error {
-	return nil
+// lineToRanges parses an input line and returns the int ranges found
+func lineToRanges(line string) ([]inclusiveRange, error) {
+	out := make([]inclusiveRange, 0, 2)
+	for _, x := range strings.Split(line, ",") {
+		elements := strings.Split(x, "-")
+		if len(elements) != 2 {
+			return nil, fmt.Errorf("malformed line %s", line)
+		}
+		start, err := strconv.Atoi(elements[0])
+		if err != nil {
+			return nil, err
+		}
+		end, err := strconv.Atoi(elements[1])
+		if err != nil {
+			return nil, err
+		}
+		out = append(out, inclusiveRange{Start: start, End: end})
+	}
+	return out, nil
+}
+
+// isSubset returns true if one range is a subset of the other
+func isSubset(left, right inclusiveRange) bool {
+	return (left.Start <= right.Start && left.End >= right.End) ||
+		(right.Start <= left.Start) && (right.End >= left.End)
+}
+
+// isOverlap returns true if either intRange overlap at all
+func isOverlap(left, right inclusiveRange) bool {
+	if left.Start <= right.Start {
+		return left.End >= right.Start
+	} else {
+		return isOverlap(right, left)
+	}
 }
--- main.go
+++ main.go
@@ -3,11 +3,11 @@ package main
 import (
 	"log"
 
-	"github.com/zenyui/aoc-22/day03"
+	"github.com/zenyui/aoc-22/day04"
 )
 
 func main() {
-	if err := day03.Run(); err != nil {
+	if err := day04.Run(); err != nil {
 		log.Fatal(err)
 	}
 }
